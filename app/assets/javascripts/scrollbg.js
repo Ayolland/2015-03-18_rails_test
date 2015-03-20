9c62f7b4a9207ba867a7b3f0bdafc80cff3e3989
Andrew Yolland <and.t.yoll@gmail.com>
2015-03-20 09:11:01 -0500

    hopefully reverted and minor css fix

---
+++ app/assets/javascripts/scrollbg.js
@@ -0,0 +1,12 @@
+var position = 0;
+
+window.onload = function() {
+  window.setInterval(moveBg, 30);
+};
+
+function moveBg() {
+  position++;
+  if (position > 190){ position = 0 };
+  var pos_string = position + "px";
+  document.body.style.backgroundPosition = pos_string + " -" + pos_string
+}
