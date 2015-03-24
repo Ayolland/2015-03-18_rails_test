74f5e89cfdc45e90c96fd20a77295793a3445333
Andrew Yolland <and.t.yoll@gmail.com>
2015-03-24 13:20:15 -0500

    commented out working js

--- app/assets/javascripts/scrollbg.js
+++ app/assets/javascripts/scrollbg.js
@@ -42,37 +42,37 @@ function randIntBtwn(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function duck(htmlElement,rSrc,lSrc,dSrc) {
-  this.div = htmlElement;
-  this.reset();
-  this.rSrc = rSrc;
-  this.lSrc = lSrc;
-  this.dSrc = dSrc;
-  this.outOfBounds = function(){
-    return true if (this.xPos < -66 || this.xPos > window.innerWidth + 66);
-    return true if (this.yPos < -66 || this.xPos > window.innerHeight + 66);
-    return false 
-  }
-  this.reset = function(){
-    this.xPos = -66;
-    this.yPos = randIntBtwn(5,window.innerHeight-60);
-    this.xVel = randIndBtwn(1,5);
-    this.yVel = 0;
-    this.yAcc = 0;
-    this.xAcc = randIndBtwn(0,4);
-    this.div.style.zIndex = randIntBtwn(0,8);
-    this.div.style.transform = this.translateStr();
-  }
-  this.translateStr = function(){
-    "translate(" + this.xPos + "px," + this.yPos +"px)"
-  }
-  this.move = function(){
-    this.xPos += this.xVel;
-    this.xVel += this.xAcc;
-    this.yPos += this.yVel;
-    this.yVel += this.yAcc;
-    this.div.style.transform = this.translateStr();
-  }
-}
+// function duck(htmlElement,rSrc,lSrc,dSrc) {
+//   this.div = htmlElement;
+//   this.reset();
+//   this.rSrc = rSrc;
+//   this.lSrc = lSrc;
+//   this.dSrc = dSrc;
+//   this.outOfBounds = function(){
+//     return true if (this.xPos < -66 || this.xPos > window.innerWidth + 66);
+//     return true if (this.yPos < -66 || this.xPos > window.innerHeight + 66);
+//     return false
+//   }
+//   this.reset = function(){
+//     this.xPos = -66;
+//     this.yPos = randIntBtwn(5,window.innerHeight-60);
+//     this.xVel = randIndBtwn(1,5);
+//     this.yVel = 0;
+//     this.yAcc = 0;
+//     this.xAcc = randIndBtwn(0,4);
+//     this.div.style.zIndex = randIntBtwn(0,8);
+//     this.div.style.transform = this.translateStr();
+//   }
+//   this.translateStr = function(){
+//     "translate(" + this.xPos + "px," + this.yPos +"px)"
+//   }
+//   this.move = function(){
+//     this.xPos += this.xVel;
+//     this.xVel += this.xAcc;
+//     this.yPos += this.yVel;
+//     this.yVel += this.yAcc;
+//     this.div.style.transform = this.translateStr();
+//   }
+// }
 
 
